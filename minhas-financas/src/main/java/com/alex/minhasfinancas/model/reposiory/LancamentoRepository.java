b576cd9bb76a63633e0c7212e2cd01c3469b2dc4
AlexsandroAn <userdeploy@WIN10-DESENV.sop.local>
2021-12-04 22:32:45 -0200

    80. Realizando as requisições de alterar status do lançamentos e saldo

--- minhas-financas/src/main/java/com/alex/minhasfinancas/model/reposiory/LancamentoRepository.java
+++ minhas-financas/src/main/java/com/alex/minhasfinancas/model/reposiory/LancamentoRepository.java
@@ -1,6 +1,7 @@
 package com.alex.minhasfinancas.model.reposiory;
 
 import com.alex.minhasfinancas.model.entity.Lancamento;
+import com.alex.minhasfinancas.model.enums.TipoLancamento;
 import java.math.BigDecimal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +12,6 @@ public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
     @Query(value
             = " select sum(l.valor) from Lancamento l join l.usuario u "
             + " where u.id = :idUsuario and l.tipo = :tipo group by u ")
-    BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") String tipo);
+    BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo);
 
 }

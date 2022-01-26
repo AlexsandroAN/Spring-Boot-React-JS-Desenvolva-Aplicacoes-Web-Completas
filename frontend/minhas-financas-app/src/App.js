2ab807231570af61a6c8d4964ef9f15ffb631159
AlexsandroAn <userdeploy@WIN10-DESENV.sop.local>
2022-01-26 09:28:12 -0300

    108. Componentizando o formulário de Login

--- frontend/minhas-financas-app/src/App.js
+++ frontend/minhas-financas-app/src/App.js
@@ -1,43 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import { render } from '@testing-library/react';
+import 'bootswatch/dist/flatly/bootstrap.css'
+import Login from './view/login'
 
 class App extends React.Component {
-
-  state = {
-    numero1: null,
-    numero2: null,
-    resultado: null
-  }
-
-  somar = () => {
-    const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2)
-    this.setState({ resultado: resultado })
-  }
-
-
-  render() {
+  render() {    
     return (
       <div>
-        <label>Primeiro Número:</label>
-        <input type="text" value={this.state.numero1}
-          onChange={(e) => this.setState({ numero1: e.target.value })} />
-        <br />
-        <label>Segundo Número:</label>
-        <input type="text" value={this.state.numero2}
-          onChange={(e) => this.setState({ numero2: e.target.value })} />
-        <br />
-        <button
-          onClick={this.somar} >
-          Somar
-        </button>
-        <br />
-        O resultado é: {this.state.resultado}
-      </div >
+        <Login/>
+      </div>
     )
+
   }
 }
-
 
 export default App;
